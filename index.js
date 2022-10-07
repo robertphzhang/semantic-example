1c8494789ae44abfaa74a7b8e0cef72c8628b4ab
Zhang <robertzh@3c06300bfed1.ant.amazon.com>
2022-10-07 11:45:21 -0400

    feat: sd

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 module.exports.hello = (name, age) => {   return `Hello ${name}! ${name} is ${age} years old!`}; 
 
 console.log("hi")
-console.log("hi")
-console.log("hi")
-console.log("hi")
-
-console.log("hi")
-
